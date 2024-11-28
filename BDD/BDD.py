b5a291e4c03f40e8674ee0cd517e367d59bad98d
Anna Nienaber <89587790+ANienaber@users.noreply.github.com>
2024-11-28 23:18:25 +0100

    removed old BDD display modes

--- BDD/BDD.py
+++ BDD/BDD.py
@@ -7,6 +7,7 @@ class BDDNode:
         self.left = left
         self.right = right
         self.value = value  # Terminal value (True or False for leaf nodes)
+        self.assignment = {}
         self.drawn = False
 
     def isLeaf(self):
@@ -39,37 +40,41 @@ class BDD:
         self.variables = variables  # List of variables
         self.expression = expression
         self.current_assignment = {}
+        self.evaluation = {} #dict of all evaluations
         self.leafs= {False: BDDNode(value=False), True:BDDNode(value=True)}
         self.root = self.build(0)  # Build the BDD starting from the first variable index
+        
 
     def build(self, var_index):
-
         # end of recursion if node is a leaf
         if var_index == len(self.variables):
             assignment = {var: val for var, val in self.current_assignment.items()}  # copies current_assignment
             value = evaluate_expression(self.expression, assignment)
-            #leaf_vals = list(leaf.value for leaf in self.leafs)
-            #if value not in leaf_vals:
-            #    self.leafs.add(BDDNode(value))
-            return BDDNode(value=value)
+            self.evaluation[tuple(assignment.items())] = value
+            leaf = BDDNode(value=value)
+            leaf.assignment = assignment  # Assign the final assignment to the leaf
+            return leaf
 
         # Create node for false subtree and true subtree
         var = self.variables[var_index]
         self.current_assignment[var] = False
         leftNode = self.build(var_index + 1)
+
         self.current_assignment[var] = True
         rightNode = self.build(var_index + 1)
 
-        return BDDNode(var=var, left=leftNode, right=rightNode)
+        node = BDDNode(var=var, left=leftNode, right=rightNode)
+        node.assignment = {var: val for var, val in self.current_assignment.items()}
+        return node
 
-    # traverses down the diagram to evaluate it
-    def evaluate(self, node, variables):
-        if node.isLeaf():
-            return node.value
-        if variables[node.var] is False:
-            return self.evaluate(node.left, variables)
-        else:
-            return self.evaluate(node.right, variables)
+    # # traverses down the diagram to evaluate it
+    # def evaluate(self, node, variables):
+    #     if node.isLeaf():
+    #         return node.value
+    #     if variables[node.var] is False:
+    #         return self.evaluate(node.left, variables)
+    #     else:
+    #         return self.evaluate(node.right, variables)
         
     def reduce(self):
         self.remove_duplicate_subtree(self.root, mem = {})
@@ -110,71 +115,9 @@ class BDD:
             if eq_child_right is not None:
                 node.right = eq_child_right
         if node.left is not None and node.right is not None and id(node.left) == id(node.right):
-            return node.right
+            if node.var in node.right.assignment:
+                del node.right.assignment[node.var]
         return None
-        
-
-    #displays BDD graphically
-    def display(self, node=None, level=0, output=None):
-        # Start from the root
-        if node is None:
-            node = self.root
-
-        indent = "      " * level  # indentation based level
-        if node.isLeaf():
-            print(node.value)
-        else:
-            print(node.var)
-            if node.left:
-                print(f"{indent}├----", end=" ")
-                self.display(node.left, level + 1)
-            if node.right:
-                print(f"{indent}└────", end=" ")
-                self.display(node.right, level + 1)
-
-    def generate_latex(self, filename="output", node=None, level = 0):
-        node = self.root          
-        out = open(f"BDD\\out\\{filename}.tex", "w")
-        out.write ("\\documentclass{article} \n\
-\\usepackage{tikz-qtree} \n\
-    \\begin{document} \n\
-        \\begin{center} \n\
-            \\tikzset{every tree node/.style={minimum width=2em,draw,circle},\
-            blank/.style={draw=none}, %nodes are round\n\
-            leaf/.style={draw, rectangle}, %leafs are square \n\
-            edge from parent/.style= \n\
-            {draw, edge from parent path={(\\tikzparentnode) -- (\\tikzchildnode)}}, \n\
-            level distance=1.5cm} \n\
-                \\begin{tikzpicture} \n\
-                    \\Tree\n\
-                   ")
-        self.generate_latex_recursive(node, level, out, False)
-        out.write("          \\end{tikzpicture} \n\
-                        \\end{center} \n\
-                    \\end{document}\n")
-        print("File generated")
-
-
-    def generate_latex_recursive(self, node, level, out, side):
-        indent = "  " * level + "                    "
-        if node.isLeaf(): 
-            if side:
-                out.write (f"\\edge[]; \\node[leaf]{{{node.value}}};\n")
-            else:             
-                out.write (f"\\edge[dashed]; \\node[leaf]{{{node.value}}};\n")
-        else:
-            out.write("[."+node.var+"\n"+indent+"\edge[dashed];\n")
-            if node.left:
-                out.write(indent)
-                self.generate_latex_recursive(node.left, level + 1, out, 0)
-            else:
-                 out.write (indent + "\\edge[blank]; \\node[blank]{};\n \\\left")
-            if node.right:
-                out.write(indent)
-                self.generate_latex_recursive(node.right, level + 1, out, 1)
-            else:
-                 out.write (indent + "\\edge[blank]; \\node[blank]{};\n \\\right")
-            out.write(indent+"]\n")
 
     def generateDot(self, filename="output", node=None):       
         node = self.root            
@@ -183,6 +126,7 @@ class BDD:
         self.generate_dot_recursive(node, out)
         out.write("}")
         print("Dot File generated")
+        self.reset_draw(self.root)
 
     def generate_dot_recursive(self, node, out):
         if not node.drawn:
@@ -216,29 +160,16 @@ class BDD:
         node.drawn = False
        
 
-    #def reduce(self):
-        #self.removeNodesWithSameChildren()
-        #self.removeDuplicateTerminalNodes()
 
-    #def removeNodesWithSameChildren(self):
-
-    #def removeDuplicateTerminalNodes(self):
-        #positiveNode = BDDNode(value=1)
-        #negativeNode = BDDNode(value=0)
-
-
-# Example:
+#Example:
 e = "(A and B) or C"
 e = "((not A or B) and (not B or A)) and ((not C or D) and (not D or C))"
 v = ['A', 'B', 'C', 'D']
 bdd = BDD(e, v)
 
 print("Binary Decision Diagram (BDD):")
-bdd.generate_latex(node = bdd.root, filename="out")
-bdd.display(bdd.root)
 bdd.generateDot(node=bdd.root, filename="out")
 bdd.reduce()
-bdd.display(bdd.root)
-bdd.generate_latex(node=bdd.root, filename="reduced_out")
-bdd.reset_draw(bdd.root)
 bdd.generateDot(node=bdd.root, filename="reduced_out")
+for k, v in bdd.evaluation.items():
+    print(f"{k}: {v}")
